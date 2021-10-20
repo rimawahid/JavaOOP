362fa5b50cc364a75b1d483485893c1b9cfd465c
Rima Akter <rimawahid3@gmail.com>
2021-10-20 19:00:14 +0600

    Start

---
+++ src/oop/Teacher.java
@@ -0,0 +1,14 @@
+
+package oop;
+
+
+public class Teacher {
+    String name,gender;
+    int phone;
+    
+    void displayInfo(){
+        System.out.println("Name : " + name);
+        System.out.println("Gender : " + gender);
+        System.out.println("Phone : " + phone);
+    }
+}
---
+++ src/oop/Test.java
@@ -0,0 +1,19 @@
+
+package oop;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher();
+        teacher1.name = "Anis";
+        teacher1.gender = "Male";
+        teacher1.phone = 0134545654;
+        teacher1.displayInfo();
+        
+        Teacher teacher2 = new Teacher();
+        teacher2.name = "Maruf";
+        teacher2.gender = "Male";
+        teacher2.phone = 0134545654;
+        teacher2.displayInfo();
+    }
+}
