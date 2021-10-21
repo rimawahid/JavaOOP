5c3dfc6cd9af857794247d5f0812c86bb185a23f
Rima Akter <rimawahid3@gmail.com>
2021-10-21 22:26:24 +0600

    static method

---
+++ src/staticMethod/StaticMethod.java
@@ -0,0 +1,13 @@
+
+package staticMethod;
+
+public class StaticMethod {
+    void display1(){
+        System.out.println("Non static method");
+        display2();
+    }
+    static void display2(){
+        
+        System.out.println("Static methos");
+    }
+}
---
+++ src/staticMethod/Test.java
@@ -0,0 +1,12 @@
+
+package staticMethod;
+
+
+public class Test {
+    public static void main(String[] args) {
+        StaticMethod sm = new StaticMethod();
+        sm.display1();
+        
+        StaticMethod.display2();
+    }
+}
