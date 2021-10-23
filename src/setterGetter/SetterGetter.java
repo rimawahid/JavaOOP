cc40499b9f8795ed4046d91359699b2a68df8fb6
Rima Akter <rimawahid3@gmail.com>
2021-10-23 21:38:09 +0600

    getter setter

---
+++ src/setterGetter/SetterGetter.java
@@ -0,0 +1,21 @@
+
+package setterGetter;
+
+public class SetterGetter {
+    private String name;
+    private int age;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/setterGetter/Test.java
@@ -0,0 +1,13 @@
+package setterGetter;
+
+public class Test {
+    public static void main(String[] args) {
+        SetterGetter p1 = new SetterGetter();
+        p1.setName("Kutu");
+        System.out.println(p1.getName());
+        p1.setAge(2);
+        System.out.println(p1.getAge());
+          
+    }
+            
+}
