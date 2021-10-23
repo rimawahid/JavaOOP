5e1c543199da85082f0e279f2eadc3aae93409d9
Rima Akter <rimawahid3@gmail.com>
2021-10-23 16:31:34 +0600

    variable lenght

---
+++ src/varargs/AddDemoTest.java
@@ -0,0 +1,10 @@
+
+package varargs;
+
+public class AddDemoTest {
+    public static void main(String[] args) {
+        AddDemo ad = new AddDemo();
+        ad.add(10,20,30);
+        ad.add(23,34,45,56,67,78);
+    }
+}
